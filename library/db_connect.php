fa51f53d97b40ec2964c5885553f19181c952183
DenisStrahov <strahov_denis@mail.ru>
2016-10-29 18:21:26 +0300

    db_connect

--- library/db_connect.php
+++ library/db_connect.php
@@ -7,8 +7,6 @@ class DB
     public static function getInstance()
     {
         if (null === static::$instance) {
-//            static::$instance = new PDO('mysql:host=mysql.hostinger.com.ua; dbname=u487236623_roz', "u487236623_denis", "ghbrjk",
-//                array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
             static::$instance = new PDO('mysql:host=localhost; dbname=roz', "root", "",
                 array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
         }
